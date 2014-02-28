d811cff7546958eedce9ba513563139b14d58b21
Joe Dytrych <me@jdytrych.co.uk>
2014-02-28 21:22:28 +0000

    Initial commit

--- content/themes/drumroll/assets/js/index.js
+++ content/themes/drumroll/assets/js/index.js
@@ -4,27 +4,73 @@
 
 /*globals jQuery, document */
 (function ($) {
-    'use strict';
+  'use strict';
 
-    var $doc = $(document);
+  FastClick.attach(document.body);
 
-    $doc.ready(function() {
+  var $doc = $(document),
+    $nav = $('header.site-head nav'),
+    $navParts = $('.site-head nav, .site-head nav > ul'),
+    $header = $('header.site-head'),
+    $navLogo = $nav.find('.logo'),
+    headerFixed = $nav.hasClass('fixed'),
+    isHome = $(document.body).hasClass('home');
 
-        $('.post-content').fitVids();
+  $doc.ready(function() {
 
-    });
+      $('.post-content').fitVids();
 
-    var $nav = $('header nav');
-    $('header nav .menu').on('click', function() {
-      $nav.toggleClass('expanded');
-    });
+  });
 
-    var $logo = $('.blog-logo img'),
-      logos = ['/assets/img/logo-l.png', '/assets/img/logo-r.png'];
+  var navExpanded = false;
+  $('header nav .menu').on('click', function() {
+    if (navExpanded) {
+      $navParts.css('height', '');
+      navExpanded = false;
+    } else {
+      var initHeight = $navParts.height();
+      $navParts.css('height', 'auto');
+      var targetHeight = $navParts.height();
+      $navParts.css('height', '');
+      setTimeout(function() {
+        $navParts.height(targetHeight);
+      }, 0);
+      navExpanded = true;
+    }
+  });
 
+  $('header nav li:not(.menu)').on('click', function() {
+    $navParts.css('height', '');
+    navExpanded = false;
+  });
+
+  var $logo = $('.blog-logo img'),
+    logos = ['/assets/img/logo-l.png', '/assets/img/logo-r.png'];
+
+  if (isHome) {
     $doc.on('scroll', function() {
-      var t = Math.floor($doc.scrollTop() / 10) % 2;
-      $logo.attr('src', logos[t]);
-    });
+      var scroll = $doc.scrollTop();
 
+      $logo.attr('src', logos[Math.floor(scroll / 20) % 2]);
+
+      // Fixed stuff:
+      if ($header.outerHeight() - 50 < scroll) {
+        if (!headerFixed) {
+          $nav.addClass('fixed');
+          setTimeout(function() {
+            $navLogo.addClass('active');
+          }, 100);
+          headerFixed = true;
+        }
+      } else {
+        if (headerFixed) {
+          $nav.removeClass('fixed');
+          setTimeout(function() {
+            $navLogo.removeClass('active');
+          }, 100);
+          headerFixed = false;
+        }
+      }
+    }).trigger('scroll');
+  }
 }(jQuery));
--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -15,7 +15,7 @@ module.exports = function (server) {
     //  Given `/plain-slug/` the req.params would be [undefined, 'plain-slug']
     //  Given `/2012/12/24/plain-slug/` the req.params would be ['2012/12/24/', 'plain-slug']
     //  Given `/plain-slug/edit/` the req.params would be [undefined, 'plain-slug', 'edit']
-    server.get(/^\/(?:)\/([0-9]{4}\/[0-9]{2}\/[0-9]{2}\/)?([^\/.]*)\/$/, frontend.single);
+    server.get(/^\/([0-9]{4}\/[0-9]{2}\/[0-9]{2}\/)?([^\/.]*)\/$/, frontend.single);
     server.get(/^\/([0-9]{4}\/[0-9]{2}\/[0-9]{2}\/)?([^\/.]*)\/edit\/$/, frontend.edit);
     server.get('/', frontend.homepage);
 };
