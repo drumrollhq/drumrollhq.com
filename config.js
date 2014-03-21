378933784c8a977695c7e69db6b3c6d2f7ba6116
Git <git@dytry.ch>
2014-03-21 21:13:34 +0000

    Update name to drumrollhq

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://godrumroll.org',
+        url: 'http://drumrollhq.com',
         mail: {},
         database: {
             client: 'sqlite3',
